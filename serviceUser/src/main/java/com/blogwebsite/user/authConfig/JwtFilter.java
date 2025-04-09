5d1f07d345f8bb56100aee3bd56684eddd929d06
Radhika Yadav <radhikayaduvanshi001@gmail.com>
2025-04-09 19:14:13 +0530

    there is no changes in jwtFilter.java  there is change in cofing file getById and getByEmail is white listed in confing after that it is working properly from backend and now user can create post by email id and user id both recently email id is implemented

--- serviceUser/src/main/java/com/blogwebsite/user/authConfig/JwtFilter.java
+++ serviceUser/src/main/java/com/blogwebsite/user/authConfig/JwtFilter.java
@@ -17,70 +17,69 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-public class JwtFilter extends OncePerRequestFilter
-{
+public class JwtFilter extends OncePerRequestFilter {
 
-	@Autowired
-	private JwtService jwtService;
-	
-	@Autowired
-	private ApplicationContext applicationContext;
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		try 
-		{
-			System.out.println("do filter internal");
-			String authHeader= request.getHeader("Authorization");
-			String token=null;
-			String userName=null;
-			String  email=null;
+    @Autowired
+    private JwtService jwtService;
 
-			System.err.println("header is here : "+authHeader);
-			
-			if(authHeader!=null && authHeader.startsWith("Bearer ")) //header prefix Bearer
-			{
-				token = authHeader.substring(7);//remove bearer
-				System.out.println("Token in do internal : "+token);
-				
-	            // Extracting username from the token
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        try {
+            System.out.println("do filter internal");
+            String authHeader = request.getHeader("Authorization");
+            String token = null;
+            String userName = null;
+            String email = null;
+
+            System.err.println("header is here : " + authHeader);
+
+            if (authHeader != null && authHeader.startsWith("Bearer ")) //header prefix Bearer
+            {
+                token = authHeader.substring(7);//remove bearer
+                System.out.println("Token in do internal : " + token);
+
+                // Extracting username from the token
 //	            userName =applicationContext.getBean(JwtService.class).extractUserName(token);
-				email= applicationContext.getBean(JwtService.class).extractUserName(token);
-				System.out.println("User name  in Dointernal : "+userName);
-				System.out.println("email in do internal filter : "+email);
-	            		//jwtService.extractUserName(token);
-			}
-			
-			// If username is extracted and there is no authentication in the current SecurityContext
-	        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-	        	
-	        	//check the username should not null and securitycontext should be null
-	        	
-	            // Loading UserDetails by username extracted from the token
-	            UserDetails userDetails = applicationContext.getBean(CustomUserService.class).loadUserByUsername(email);
+                email = applicationContext.getBean(JwtService.class).extractUserName(token);
+                System.out.println("User name  in Dointernal : " + userName);
+                System.out.println("email in do internal filter : " + email);
+                //jwtService.extractUserName(token);
+            }
 
-				System.err.println("USer Details : "+userDetails);
-	           // System.out.println(userDetails);
-	            // Validating the token with loaded UserDetails
-	            if (jwtService.verifyTocken(token, userDetails)) {
-	            	
-	                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-	             
-	                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            // If username is extracted and there is no authentication in the current SecurityContext
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-					System.err.println("");
+                //check the username should not null and securitycontext should be null
 
-	                SecurityContextHolder.getContext().setAuthentication(authToken);
-				}
-	        }
-	        filterChain.doFilter(request, response);	
-		} catch (Exception e) {
-			System.out.println("catch block from jwt filter..");
-			response.setContentType("application/json");
+                // Loading UserDetails by username extracted from the token
+                UserDetails userDetails = applicationContext.getBean(CustomUserService.class).loadUserByUsername(email);
+
+                System.err.println("USer Details : " + userDetails);
+                // System.out.println(userDetails);
+                // Validating the token with loaded UserDetails
+                if (jwtService.verifyTocken(token, userDetails)) {
+
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    System.err.println("");
+
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            System.out.println("catch block from jwt filter..");
+            response.setContentType("application/json");
 //			new ObjectMapper().writeValue(response.getOutputStream(), new ErrorResponse(e.getMessage(), 404));
-		}
-		
-	}
+        }
+
+    }
 
 }
